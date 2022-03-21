620636c463b5b6a30b2e18a6b662108dca5f75fb
Dany <dleon@klikealo.net>
2022-03-20 22:24:51 -0500

    Fix

--- src/components/custom-navbar/custom-navbar.tsx
+++ src/components/custom-navbar/custom-navbar.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import {Container, Nav, Navbar} from "react-bootstrap";
-import {Link, NavLink} from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const CustomNavbar = (): React.ReactElement => {
   return (
--- src/pages/clients-create/index-clients-create.tsx
+++ src/pages/clients-create/index-clients-create.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import {Button, Col, Container, Row, Table} from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import TitlePage from "../../components/shared/title-page";
-import {Link} from "react-router-dom";
 import ClientForm from "../../components/clients/client-form";
 
 const IndexClientsCreate = (): React.ReactElement => {
